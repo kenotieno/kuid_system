22a61ef8e73b3c7b1a960c6ea3a2b7300cb97eb4
Kennedy Otieno <kennedyomoro16@gmail.com>
2025-05-07 13:48:41 +0300

    Switch to SQLite for free deployment

--- kuid_system/kuid_system/settings.py
+++ kuid_system/kuid_system/settings.py
@@ -95,9 +95,11 @@ WSGI_APPLICATION = 'kuid_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3')
 }
+
 
 
 # Password validation
